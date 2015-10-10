91fdb7e7594e9cb59cfa332f3101d5780b037b87
Hugo Fernandes <hugo.medeiros.fernandes@gmail.com>
2015-10-10 08:53:12 -0300

    Projeto com Graficos

--- CashSafe/app/src/main/java/com/cashsafe/cashsafe/PrincipalActivity.java
+++ CashSafe/app/src/main/java/com/cashsafe/cashsafe/PrincipalActivity.java
@@ -1,19 +1,61 @@
 package com.cashsafe.cashsafe;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+
 public class PrincipalActivity extends AppCompatActivity {
+
+
+    private PieChart graficoDespesas;
+    private PieChart graficoReceitas;
+    float[] yDataDespesas = { 15, 10 };
+    String[] xDataDespesas = { "Taxi", "Aluguel"};
+    float[] yDataReceitas = { 20, 2 };
+    String[] xDataReceitas = { "Salario", "Presente"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
         Intent intent = getIntent();
+
+        // configure pie chart
+        graficoDespesas = (PieChart) findViewById(R.id.categoriaschart);
+        graficoDespesas.setDrawHoleEnabled(false);
+        graficoDespesas.setDescription("");
+        graficoDespesas.setRotationEnabled(false);
+
+        graficoReceitas = (PieChart) findViewById(R.id.receitaschart);
+        graficoReceitas.setDrawHoleEnabled(false);
+        graficoReceitas.setDescription("");
+        graficoReceitas.setRotationEnabled(false);
+
+        adicionarDadosDespesas();
+        adicionarDadosReceitas();
+        Legend l = graficoDespesas.getLegend();
+        l.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
+        l.setXEntrySpace(7);
+        l.setYEntrySpace(5);
+
+        l = graficoReceitas.getLegend();
+        l.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
+        l.setXEntrySpace(7);
+        l.setYEntrySpace(5);
 
     }
 
@@ -62,5 +104,109 @@ public class PrincipalActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void adicionarDadosDespesas() {
+        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
+
+        for (int i = 0; i < yDataDespesas.length; i++)
+            yVals1.add(new Entry(yDataDespesas[i], i));
+
+        ArrayList<String> xVals = new ArrayList<String>();
+
+        for (int i = 0; i < xDataDespesas.length; i++)
+            xVals.add(xDataDespesas[i]);
+
+        // create pie data set
+        PieDataSet dataSet = new PieDataSet(yVals1, "Despesas");
+        dataSet.setSliceSpace(3);
+        dataSet.setSelectionShift(5);
+
+        // add many colors
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+        for (int c : ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);
+
+        colors.add(ColorTemplate.getHoloBlue());
+        dataSet.setColors(colors);
+
+        // instantiate pie data object now
+        PieData data = new PieData(xVals, dataSet);
+        data.setValueTextSize(11f);
+        data.setValueTextColor(Color.GRAY);
+
+        graficoDespesas.setData(data);
+
+        // undo all highlights
+        graficoDespesas.highlightValues(null);
+
+        // update pie chart
+        graficoDespesas.invalidate();
+        graficoDespesas.getLegend().setEnabled(false);
+    }
+
+    private void adicionarDadosReceitas() {
+        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
+
+        for (int i = 0; i < yDataReceitas.length; i++)
+            yVals1.add(new Entry(yDataReceitas[i], i));
+
+        ArrayList<String> xVals = new ArrayList<String>();
+
+        for (int i = 0; i < xDataReceitas.length; i++)
+            xVals.add(xDataReceitas[i]);
+
+        // create pie data set
+        PieDataSet dataSet = new PieDataSet(yVals1, "Receitas");
+        dataSet.setSliceSpace(3);
+        dataSet.setSelectionShift(5);
+
+        // add many colors
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+        for (int c : ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);
+
+        colors.add(ColorTemplate.getHoloBlue());
+        dataSet.setColors(colors);
+
+        // instantiate pie data object now
+        PieData data = new PieData(xVals, dataSet);
+        data.setValueTextSize(11f);
+        data.setValueTextColor(Color.GRAY);
+
+        graficoReceitas.setData(data);
+
+        // undo all highlights
+        graficoReceitas.highlightValues(null);
+
+        // update pie chart
+        graficoReceitas.invalidate();
+        graficoReceitas.getLegend().setEnabled(false);
     }
 }
